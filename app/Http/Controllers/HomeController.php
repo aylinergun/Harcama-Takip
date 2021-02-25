99d7b86d5d471aecdd3a0b27cbe5c4ce24b5b040
aylinergun <aylinergun@yahoo.com>
2021-02-25 17:01:04 +0300

    max,min,last expenditures

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,32 +3,45 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
 use App\Models\Category;
 use App\Models\Expenditure;
 
-
 class HomeController extends Controller
-  {
-  //    public function __construct(){
-    //   $this->middleware('auth');
-  //}
+      {
+         public function __construct()
+         {
+           $this->middleware('auth');
+         }
 
-     public function addExpenditures()
-     {
-       $categories=Category::all();
-       $expenditures=Expenditure::with('Category');
+         public function index()
+      {
+            $categories=Category::all();
+            $expenditures=Expenditure::with('Category');
 
-       return view('create')->with(compact('categories', 'expenditures'));
+            $maxExpenditure=DB::table('expenditures')
+                            ->orderBy('total','desc')
+                            ->first();
+
+                    //  return "En çok harcama " .$maxExpenditure->total ;
+                      //return view('create')->with(compact('categories','maxExpenditure'));
+
+             $minExpenditure=DB::table('expenditures')
+                              ->orderBy('total','asc')
+                              ->first();
+
+                      //return "En az harcama " .$minExpenditure->total;
+              //kategoriyle
+              $lastExpenditure=Expenditure::with('category')
+                              ->orderBy('date','desc')
+                              ->first();
+
+              return view('home')->with(compact('categories','expenditures','maxExpenditure','minExpenditure','lastExpenditure'));
+
+
+
+      }
     }
-
-    public function listExpenditures()
-    {
-       $categories=Category::all();
-       $expenditures=Expenditure::all();
-
-       return view('home')->with(compact('categories','expenditures'));
-    }
-
-  }
 
 ?>
