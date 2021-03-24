02b22ae47daa7034d0ac704087dd7f8435b48441
aylinergun <aylinergun@yahoo.com>
2021-03-24 23:50:35 +0300

    Max-Min ExpenditureComponent.vue

---
+++ resources/js/components/MaxExpenditureComponent.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="col-sm">
+          <div class="col-md-12 col-md-offset-0">
+            <div class="panel panel-default">
+              <div class="panel-body" align="center"><strong>En çok harcama yapılan ay</strong></div>
+                <div align="center">
+                  <tr>
+                   <td>AY: </td>
+                   <td align="center">{{maxExpenditure.data.month}}</td>
+                  <br>
+                  <td>TUTAR: </td>
+                  <td align="center">{{maxExpenditure.data.total}}</td>
+                  </tr>
+                </div>
+              </div>
+            </div>
+          </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+    export default{
+      mounted(){
+          this.loadMaxExpenditure();
+      },
+      props:[
+      ],
+      data(){
+        return{
+          maxExpenditure:""
+        }
+      },
+      methods:{
+        loadMaxExpenditure(){
+         axios.get('/api/v1/max-expenditure')
+          .then((response)=>{
+            this.maxExpenditure=response.data;
+          })
+          .catch((error)=>{
+            console.log('Error:',error);
+          });
+        },
+      }
+    }
+</script>
---
+++ resources/js/components/MinExpenditureComponent.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="col-sm">
+        <div class="panel panel-default">
+          <div class="panel-body" align="center"><strong>En az harcama yapılan ay</strong></div>
+            <div align="center">
+              <tr>
+               <td>AY: </td>
+               <td align="center">{{minExpenditure.data.month}}</td>
+              <td>TUTAR: </td>
+              <td align="center">{{minExpenditure.data.total}}</td>
+              </tr>
+            </div>
+        </div>
+      </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+    export default{
+      mounted(){
+        this.loadMinExpenditure();;
+      },
+    data(){
+      return{
+        minExpenditure:""
+      }
+    },
+    methods:{
+      loadMinExpenditure(){
+        axios.get('/api/v1/min-expenditure')
+          .then((response)=>{
+            this.minExpenditure=response.data;
+      })
+            .catch((error)=>{
+              console.log('Error:',error);
+          });
+      },
+    }
+  }
+</script>
